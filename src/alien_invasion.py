4c791a25b91a39eb1bc25708711e8eb24690db76
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2024-04-09 21:34:40 +0200

    created fleet of alien

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 from settings import Settings
 from alien import Alien
 
+from random import randint
+
 
 class AlienInvasion:
     """Class to manage game assets and behavior"""
@@ -21,10 +23,28 @@ class AlienInvasion:
 
         self._create_fleet()
 
+    def _create_alien(self, position_x, position_y):
+        """Create an alien group and add it to the row"""
+        new_alien = Alien(self)
+        new_alien.x = position_x
+        new_alien.rect.x = position_x
+        new_alien.rect.y = position_y
+        self.aliens.add(new_alien)
+
     def _create_fleet(self):
-        """Function to create fleet"""
+        """Function to create fleet of aliens"""
         alien = Alien(self)
         self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < self.settings.screen_height - 3 * alien_height:
+            while current_x < self.settings.screen_width - 2 * alien.rect.width:
+                self._create_alien(current_x, current_y) if randint(0, 10) >= 3 else None
+                current_x += 2 * alien_width
+
+            current_x = alien_width
+            current_y += 2 * alien_height
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
