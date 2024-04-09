1841c2681d10e9a5134a8522940e363f418eb106
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2024-04-09 23:17:03 +0200

    added movement to the aliens

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -54,9 +54,29 @@ class AlienInvasion:
 
         pygame.display.flip()
 
+    def _update_aliens(self):
+        """Function to update the aliens in the fleet"""
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _change_fleet_direction(self):
+        """Function to change the fleet direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+    def _check_fleet_edges(self):
+        """Function to check if any aliens have reached the edges"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
--- src/settings.py
+++ src/settings.py
@@ -9,4 +9,10 @@ class Settings:
         self.bg_color = (255, 255, 255)
 
         # Game settings
-        self. randomness = False
+        self. randomness = True
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction: 1 -> right, -1 -> left
+        self.fleet_direction = 1
