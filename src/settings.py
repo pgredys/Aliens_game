fd3a5a5cdc42ed89bed7d7905a14e4364a364caf
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2024-04-09 21:40:29 +0200

    added better randomness

--- src/settings.py
+++ src/settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255, 255, 255)
+
+        # Game settings
+        self. randomness = False
