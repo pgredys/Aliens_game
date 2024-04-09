0fae5a5bbef000816629b86f9bace989dff46b1e
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2024-04-09 23:28:41 +0200

    adjustments for HD resolution

--- src/settings.py
+++ src/settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """Initialize the game settings"""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (255, 255, 255)
 
         # Game settings
