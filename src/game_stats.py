8ce3a712bba7ca6449b496a9f9c21a989732cb12
Przemysław Grędys <gredys.przemyslaw@gmail.com>
2024-04-10 23:18:06 +0200

    added score attribute to game stats

--- src/game_stats.py
+++ src/game_stats.py
@@ -6,11 +6,18 @@ class GameStats:
 
     def __init__(self, ai_game):
         """Initialize statistics"""
+        self.level = None
+        self.score = None
         self.ships_left = None
         self.settings = ai_game.settings
         self.reset_stats()
+        
+        self.high_score = 0
 
     def reset_stats(self):
         """Reset stats"""
         self.ships_left = self.settings.ships_limit
+        
+        self.score = 0
+        self.level = 1
 
